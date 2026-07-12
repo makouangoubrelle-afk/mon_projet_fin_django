e1f4962ffe393c12d2afacf48a40f9fc26e27af2
makouangoubrelle <makouangoubrelle@gmail.com>
2026-07-12 16:28:04 +0100

    Redirect root URL to /api/ to avoid 404 on Render

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from api import api
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', api.urls),
 ]
